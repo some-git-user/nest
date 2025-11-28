7bb4e82cb7baa507801c91f7e9aaa396a417fc40
some-git-user <pennywise@geekmail.de>
2025-11-28 20:46:44 +0100

    add nagios

---
+++ src/lib/nagios.ts
@@ -0,0 +1,45 @@
+import {
+  NagiosReturnMessage,
+  nagiosReturnValuesEnum,
+  PerformanceData,
+} from "@/@types/nagios";
+
+export const createNagiosReturnMessage = (
+  message: string,
+  code: nagiosReturnValuesEnum,
+  performanceData?: PerformanceData | PerformanceData[]
+): NagiosReturnMessage => {
+  const nagiosReturnMessage: NagiosReturnMessage = {
+    message,
+    code,
+  };
+
+  if (performanceData) {
+    console.log(performanceData);
+    if (!Array.isArray(performanceData)) {
+      performanceData = [performanceData];
+    }
+
+    if (performanceData.every((perfData) => perfData)) {
+      nagiosReturnMessage.performanceData = performanceData
+        .flatMap(
+          (perfData) =>
+            `${perfData.label ? `'${perfData.label}':` : ""}${
+              perfData.value
+                ? `${perfData.value}${perfData.uom ? `${perfData.uom}` : ""}`
+                : ""
+            }${perfData.warn ? `;WARN=${perfData.warn}` : ""}${
+              perfData.crit ? `;CRIT=${perfData.crit}` : ""
+            }${perfData.min ? `;MIN=${perfData.min}` : ""}${
+              perfData.max ? `;MAX=${perfData.max}` : ""
+            }`
+        )
+        .join(" ")
+        .trimStart();
+    } else {
+      console.error(`Error parsing performance data: ${performanceData}`);
+    }
+  }
+
+  return nagiosReturnMessage;
+};
