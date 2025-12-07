14dc746f8d6d32f711c7dee6e26dfd15c1abe2af
some-git-user <pennywise@geekmail.de>
2025-12-07 22:39:37 +0100

    add docs

--- script/check_nest.sh
+++ script/check_nest.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# Usage: ./check_nest.sh check-test nagiosReturnMessage=Test nagiosReturnValue=1 performanceData=true
+# Script to check the status of the Nest Nagios REST API
+# This script must be run from the Nagios server. It must be added to the Nagios server as a command.
+# Check your Nagios installation for the correct plugin directory.
+ 
+# Usage: ./check_nest.sh <command> [parameters]
+# Example: ./check_nest.sh check-test nagiosReturnMessage=Test nagiosReturnValue=1 performanceData=true
 
 # Check if jq is installed
 if ! command -v jq &> /dev/null; then
@@ -9,6 +14,10 @@ if ! command -v jq &> /dev/null; then
 fi
 
 # Function to build parameters for curl
+# Builds parameters for curl from the given arguments
+# Example: ./check_nest.sh check-test nagiosReturnMessage=Test nagiosReturnValue=1 performanceData=true
+# Will generate: -d "nagiosReturnMessage=Test" -d "nagiosReturnValue=1" -d "performanceData=true"
+# Parameters will be passed to curl as query parameters.
 build_parameters() {
     local params=""
     for param in "${@:2}"; do
@@ -18,11 +27,29 @@ build_parameters() {
 }
 
 # Function to build the URL
+# Build the URL for the curl request
+# $1 is the command to call on the Nest API
+# Example: build_url "check-test"
+# Will generate: http://localhost:5000/check-test
 build_url() {
     echo "http://localhost:5000/$1"
 }
 
 # Function to parse JSON response
+# Parse the JSON response from the Nest API
+# 
+# $1 is the JSON response from the Nest API
+# 
+# This function parses the JSON response and formats it according to Nagios standards.
+# 
+# The function will output the following format:
+# 
+# message | code=code;performanceData
+# 
+# Where:
+#   - message is the message returned by the Nest API
+#   - code is the code returned by the Nest API
+#   - performanceData is the performance data returned by the Nest API, if any
 parse_json() {
     local response="$1"
     message=$(echo "$response" | jq -r '.message')
