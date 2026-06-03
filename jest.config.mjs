5e4d831a70516f56c60cc80720624186a2a7434d
some-git-user <pennywise@geekmail.de>
2026-06-03 21:46:34 +0200

    feat: add coverage testing script and enforce coverage thresholds in Jest configuration

--- jest.config.mjs
+++ jest.config.mjs
@@ -2,6 +2,14 @@ export default {
 	roots: ['<rootDir>/src', '<rootDir>/plugins'],
 	coveragePathIgnorePatterns: ['<rootDir>/plugins/plugin-cache/'],
 	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+	coverageThreshold: {
+		global: {
+			branches: 100,
+			functions: 100,
+			lines: 100,
+			statements: 100,
+		},
+	},
 	testMatch: [
 		'**/__tests__/**/*.+(ts|tsx|js)',
 		'**/?(*.)+(spec|test).+(ts|tsx|js)',
